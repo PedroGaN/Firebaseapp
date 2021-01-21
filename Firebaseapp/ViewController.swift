e38dde97ec2c63bf1788f60c1946e3c555eb2098
PedroGaN <pgarcianavalon@gmail.com>
2021-01-21 18:13:08 +0000

    google

--- Firebaseapp/ViewController.swift
+++ Firebaseapp/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var crash: UIButton!
+    
+    @IBAction func crash(_ sender: Any) {
+        
+        fatalError()
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
